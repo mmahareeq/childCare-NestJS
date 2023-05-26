f26171c0177d19da3efe701548e5356fdfe432d9
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-05-26 12:28:10 +0300

    implement a login feature

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { UsersModule } from 'src/users/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,  JwtModule.register({
+    signOptions: { expiresIn: '24h' },
+  }),],
   controllers: [AuthController],
   providers: [AuthService]
 })
